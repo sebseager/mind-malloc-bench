0be416cdfbadd22cbc396d317d75d9045497afe0
Seager Seb <sjs264@zoo.cs.yale.edu>
2023-04-11 03:16:04 -0400

    strace for linux

--- tests/mmap_harness.sh
+++ tests/mmap_harness.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-strace ./a.out 1000000 100 100000
+strace -e trace=memory ./a.out 1000000 100 100000 2> strace.out > prog.out
 # sudo dtruss -b 100 -f -t mmap ./a.out 10000000 100 100000
