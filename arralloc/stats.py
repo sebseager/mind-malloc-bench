953cf645633e04a7af23baf4781ac346bb9047cc
Seager Seb <sjs264@zoo.cs.yale.edu>
2023-04-22 17:24:20 -0400

    adding fragmentation math

--- arralloc/stats.py
+++ arralloc/stats.py
@@ -1,7 +1,12 @@
 from argparse import ArgumentParser
+import numpy as np
 from pathlib import Path
 import pandas as pd
 import regex as re
+from matplotlib import pyplot as plt
+from decimal import Decimal
+
+NS_PER_SEC = 1000000000
 
 # Each file is a list of memory-related calls recorded by strace
 # for a single run of arralloc. The format per-line is:
@@ -11,7 +16,10 @@ import regex as re
 def parse_strace(*files, out_file=None):
     dicts = []
     for fp in files:
-        it = int(re.search(r"\d+", fp.name).group())  # get run number from file name
+        try:
+            it = int(re.search(r"\d+", fp.name).group())  # get run number from name
+        except:
+            it = -1
         with open(fp, "r") as f:
             for line in f:
                 line = line.strip()
@@ -25,7 +33,7 @@ def parse_strace(*files, out_file=None):
                     d = {"run": it}
                     for i in range(n_elts):
                         if i == 0:
-                            d["timestamp"] = float(elts[i])
+                            d["timestamp"] = Decimal(elts[i])
                         elif i == 1:
                             d["call"] = elts[i].split("(")[0]
                         elif i == 2:
@@ -36,13 +44,14 @@ def parse_strace(*files, out_file=None):
                         if elts[i] == "=":
                             d["return"] = elts[i + 1]  # as hex string
                             # drop < and > surrounding elapsed time
-                            d["elapsed"] = float(elts[i + 2][1:-1])
+                            d["elapsed"] = Decimal(elts[i + 2][1:-1])
                 except:
                     print(f"error parsing line: {line}")
                     continue
                 dicts.append(d)
     
     df = pd.DataFrame(dicts)
+    df = df.sort_values(by=["run", "timestamp"])
     if out_file is not None:
         df.to_csv(out_file, index=False, sep="\t")
     return df
@@ -57,9 +66,11 @@ def parse_strace(*files, out_file=None):
 def parse_memtest(*files, out_file=None):
     dicts = []
     for fp in files:
-        it = int(re.search(r"\d+", fp.name).group())  # get run number from file name
+        try:
+            it = int(re.search(r"\d+", fp.name).group())  # get run number from name
+        except:
+            it = -1
         with open(fp, "r") as f:
-            elapsed = f.readline().strip().split()[1]  # TODO: unused right now
             header = f.readline().strip().split()
             for line in f:
                 line = line.strip()
@@ -70,13 +81,17 @@ def parse_memtest(*files, out_file=None):
                 try:
                     d = {"run": it}
                     for i in range(n_elts):
-                        d[header[i]] = int(elts[i])
+                        try:
+                            d[header[i]] = int(elts[i])
+                        except:
+                            d[header[i]] = Decimal(elts[i])
                 except:
                     print(f"error parsing line: {line}")
                     continue
                 dicts.append(d)
     
     df = pd.DataFrame(dicts)
+    df = df.sort_values(by=["run", "round"])
     if out_file is not None:
         df.to_csv(out_file, index=False, sep="\t")
     return df
@@ -90,9 +105,9 @@ def strace_stats(df, out_file=None):
         stats["n_mmap"] = run_df[run_df["call"] == "mmap"].shape[0]
         stats["n_munmap"] = run_df[run_df["call"] == "munmap"].shape[0]
         stats["n_total"] = stats["n_mmap"] + stats["n_munmap"]
-        stats["sz_mmap"] = run_df[run_df["call"] == "mmap"]["size"].sum()
-        stats["sz_munmap"] = run_df[run_df["call"] == "munmap"]["size"].sum()
-        stats["sz_net"] = stats["sz_mmap"] - stats["sz_munmap"]
+        stats["sz_mmap"] = int(run_df[run_df["call"] == "mmap"]["size"].sum())
+        stats["sz_munmap"] = int(run_df[run_df["call"] == "munmap"]["size"].sum())
+        stats["sz_net"] = int(stats["sz_mmap"] - stats["sz_munmap"])
         stats["secs_mmap"] = run_df[run_df["call"] == "mmap"]["elapsed"].sum()
         stats["secs_munmap"] = run_df[run_df["call"] == "munmap"]["elapsed"].sum()
         stats["secs_total"] = stats["secs_mmap"] + stats["secs_munmap"]
@@ -101,7 +116,7 @@ def strace_stats(df, out_file=None):
     df = pd.DataFrame(all_runs)
     if out_file is not None:
         df.to_csv(out_file, index=False, sep="\t")
-    print("strace stats:")
+    print("kernel-side stats:")
     print(df.to_string(index=False))
     print()
     return df
@@ -113,17 +128,16 @@ def memtest_stats(df, out_file=None):
         stats = {"run": run}
         run_df = df[df["run"] == run]
         stats["n_allocs"] = run_df["allocs"].sum()
-        stats["n_frees"] = run_df["frees"].sum()
-        stats["n_total"] = stats["n_allocs"] + stats["n_frees"]
         stats["sz_allocs"] = run_df["total_bytes"].sum()
-        stats["sz_frees"] = stats["sz_allocs"] - run_df["current_bytes"].sum()
-        stats["sz_leaked"] = stats["sz_allocs"] - stats["sz_frees"]
+        ns_start = run_df["alloc_start_time"] * NS_PER_SEC
+        ns_end = run_df["alloc_end_time"] * NS_PER_SEC
+        stats["alloc_secs"] = ((ns_end - ns_start) / NS_PER_SEC).sum()
         all_runs.append(stats)
     
     df = pd.DataFrame(all_runs)
     if out_file is not None:
         df.to_csv(out_file, index=False, sep="\t")
-    print("memtest stats:")
+    print("user-side stats:")
     print(df.to_string(index=False))
     print()
     return df
@@ -139,29 +153,126 @@ def summary_stats(strace_stats_df, memtest_stats_df, out_file=None):
         if srun_row.shape[0] == 0 or mrun_row.shape[0] == 0:
             print("skipping summary for run", run)
             continue
-        # total number of mmap/munmap calls -- lower is better
-        stats["n_syscalls"] = int(srun_row["n_total"])
-        # malloc calls : mmap calls -- lower is better
-        stats["mmap_freq"] = mrun_row["n_allocs"] / srun_row["n_mmap"]
-        # number of mallocs/frees per kernel call -- higher is better
-        stats["call_eff"] = mrun_row["n_total"] / srun_row["n_total"]
-        # bytes used by program : bytes given by kernel
-        # e.g. allocator could give 1GB for a 1KB allocation to reduce syscalls
-        stats["mmap_util"] = mrun_row["sz_allocs"] / srun_row["sz_mmap"]
-        # bytes unmapped by kernel : bytes freed by program
-        # e.g. allocator could never reclaim memory to reduce syscalls
-        stats["munmap_recl"] = srun_row["sz_munmap"] / mrun_row["sz_frees"]
-        # overall memory overhead of allocator: (malloc - free) / (mmap - munmap)
-        stats["mem_overhead"] = mrun_row["sz_leaked"] / srun_row["sz_net"]
+
+        # total number of mmap+munmap calls
+        # absolute number, lower is better
+        stats["n_mmap"] = int(srun_row["n_mmap"])
+
+        # mmap calls : malloc calls
+        # ~1 is worst, lower is better
+        stats["mmap_eff"] = srun_row["n_mmap"] / mrun_row["n_allocs"]
+
+        # size mmap'd : size mallocd'd
+        # gives fragmentation -- sense of how much memory is wasted
+        # lower is better, 1 is best
+        # stats["mmap_frag"] = srun_row["sz_mmap"] / mrun_row["sz_allocs"]
+
+        # total elapsed time in mmap : number of mallocs
+        # gives average time per malloc
+        # lower is better
+        stats["mmap_secs_per_call"] = srun_row["secs_mmap"] / mrun_row["n_allocs"]
+
+        # total elapsed time in mmap : bytes allocated
+        # gives average time per byte allocated
+        # lower is better
+        # stats["mmap_secs_per_byte"] = srun_row["secs_mmap"] / mrun_row["sz_allocs"]
+
         all_runs.append(stats)
 
     df = pd.DataFrame(all_runs)
+    df = df.sort_values(by=["run"])
     if out_file is not None:
         df.to_csv(out_file, index=False, sep="\t")
     print("summary stats:")
     print(df.to_string(index=False))
     print()
     return df
+
+
+# Modify memtest_df to add two columns: cumul_mmap_bytes and cumul_munmap_bytes.
+# These are the cumulative number of bytes mmap'd and munmap'd, respectively, up to
+# and including the given round. We need this because if the user frees a bunch 
+# of memory in user space, not all of it may be unmapped by kernel.
+def calc_frag_cols(strace_df, memtest_df, out_file=None):
+    # convert timestamps
+    memtest_df["alloc_end_time_ns"] = memtest_df["alloc_end_time"] * NS_PER_SEC
+    strace_df["timestamp_ns"] = strace_df["timestamp"] * NS_PER_SEC
+
+    # filter strace_df
+    strace_tmp = strace_df[strace_df["call"].isin(["mmap", "munmap"])]
+    strace_tmp = strace_tmp.sort_values(by=["timestamp_ns"])  # should be redundant but
+    
+    mrow_i = 0
+    mmap_cumsum = 0
+    munmap_cumsum = 0
+    for srow in strace_tmp.itertuples(index=False):
+        if srow.timestamp_ns > memtest_df.iloc[mrow_i].alloc_end_time_ns
+            memtest_df.loc[mrow_i, "cumul_mmap_bytes"] = mmap_cumsum
+            memtest_df.loc[mrow_i, "cumul_munmap_bytes"] = munmap_cumsum
+            mrow_i += 1
+            if mrow_i >= memtest_df.shape[0]:
+                break
+        if srow.call == "mmap":
+            mmap_cumsum += srow.size
+        elif srow.call == "munmap":
+            munmap_cumsum += srow.size
+        else:
+            continue
+
+    # clean up
+    memtest_df.drop(columns=["alloc_end_time_ns"], inplace=True)
+
+    # calculate fragmentation
+    memtest_df["frag"] = memtest_df["cumul_mmap_bytes"] / memtest_df["total_bytes"]
+    
+    print("detailed user-side stats with fragmentation:")
+    print(memtest_df.to_string(index=False))
+    print()
+    if out_file is not None:
+        memtest_df.to_csv(out_file, index=False, sep="\t")
+
+
+# For each run, plot fragmentation over time.
+# Fragmentation is the number of bytes mmap'd over number of bytes malloc'd (1 is best
+# and higher is worse). Because the kernel does not necessarily need to reclaim all 
+# memory freed with munmap, we need to do this over time.
+def plot_frag(strace_df, memtest_df, out_path):
+    # # add two calculated columns to memtest_df
+    # # cumul_mmap_bytes = cumulative mmap'd bytes
+    # # cumul_munmap_bytes = cumulative munmap'd bytes
+    # mmap_mask = strace_df["call"] == "mmap"
+    # munmap_mask = strace_df["call"] == "munmap"
+    # strace_df["timestamp_ns"] = strace_df["timestamp"] * NS_PER_SEC
+    # for row in memtest_df.itertuples():
+    #     # compare nanoseconds for both
+    #     end_ns = row.alloc_end_time * NS_PER_SEC
+    #     end_mask = strace_df["timestamp_ns"] <= end_ns
+    #     # get all mmap calls up to alloc_end_time
+    #     cumul_mmap_bytes = strace_df[mmap_mask & end_mask]["size"].sum()
+    #     # get all munmap calls up to alloc_end_time
+    #     cumul_munmap_bytes = strace_df[munmap_mask & end_mask]["size"].sum()
+    #     # add to memtest_df
+    #     memtest_df.loc[row.Index, "cumul_mmap_bytes"] = cumul_mmap_bytes
+    #     memtest_df.loc[row.Index, "cumul_munmap_bytes"] = cumul_munmap_bytes
+    
+    # # print for debugging
+    # print("detailed user-side stats with fragmentation:")
+    # print(memtest_df.to_string(index=False))
+    # print()
+    
+    # plot fragmentation over time (x axis is rounds)
+    # for the nth row, frag = (cumul_mmap_bytes - cumul_munmap_bytes) / total_bytes
+    plt.figure()
+    y = memtest_df["cumul_mmap_bytes"] - memtest_df["cumul_munmap_bytes"]
+    y /= memtest_df["total_bytes"]
+    x = memtest_df["alloc_start_time"]
+    plt.scatter(x, y)
+    plt.xlabel("time")
+    plt.ylabel("fragmentation")
+    plt.grid(True, which="both")
+    plt.title("Fragmentation over time")
+    plt.savefig(out_path)
+    plt.close()
 
 
 def parse_args():
@@ -184,17 +295,26 @@ def main():
     args = parse_args()
     
     # read strace files (strace_*.out) into df
-    sfiles = [f for f in args.f if re.match(r"strace_\d+\.out", f.name)]
+    
+    # regex expression matching strace*.out
+    r = re.compile(r"strace.*\.out")
+
+    sfiles = [f for f in args.f if re.match(r"strace.*\.out", f.name)]
     sdata_df = parse_strace(*sfiles, out_file=args.o / "strace_all.tsv")
 
     # read memtest files (prog_*.out) into df
-    mfiles = [f for f in args.f if re.match(r"prog_\d+\.out", f.name)]
+    mfiles = [f for f in args.f if re.match(r"prog.*\.out", f.name)]
     mdata_df = parse_memtest(*mfiles, out_file=args.o / "memtest_all.tsv")
     
     # run analyses
     sstats_df = strace_stats(sdata_df, out_file=args.o / "strace_stats.tsv")
     mstats_df = memtest_stats(mdata_df, out_file=args.o / "memtest_stats.tsv")
+    calc_frag_cols(sdata_df, mdata_df, out_file=args.o / "memtest_all.tsv")  # in-place
     summary_stats(sstats_df, mstats_df, out_file=args.o / "summary_stats.tsv")
+
+    # plots
+    if args.plot:
+        plot_frag(sdata_df, mdata_df, args.o / "strace_plot.png")
 
 
 if __name__ == "__main__":
